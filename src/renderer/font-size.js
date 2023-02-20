b0ef6672f8dd55bb5180fd23de3beed0b00fff4e
sun128764 <sun128764@gmail.com>
2023-02-20 12:04:59 +0800

    Update font-size.js
    Fix font size for Noto Sans ONLY!

--- src/renderer/font-size.js
+++ src/renderer/font-size.js
@@ -1,6 +1,6 @@
-export const $fixFontSize = document.createElement('div');
-$fixFontSize.className = 'ASS-fix-font-size';
-$fixFontSize.textContent = 'M';
+export const $fixFontSize = document.createElement("div");
+$fixFontSize.className = "ASS-fix-font-size";
+$fixFontSize.textContent = "M";
 
 const cache = Object.create(null);
 
@@ -8,7 +8,16 @@ export function getRealFontSize(fn, fs) {
   const key = `${fn}-${fs}`;
   if (!cache[key]) {
     $fixFontSize.style.cssText = `line-height:normal;font-size:${fs}px;font-family:"${fn}",Arial;`;
-    cache[key] = fs * fs / $fixFontSize.clientHeight;
+    let scale = 1;
+    switch (fn) {
+      case "Noto Sans":
+        scale = 0.77;
+        break;
+      default:
+        scale = 1;
+        break;
+    }
+    cache[key] = ((fs * fs) / $fixFontSize.clientHeight) * scale;
   }
   return cache[key];
 }
